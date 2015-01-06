1817c16c16776159b4bbf435ecded15b53e21678
Tuan Nguyen <tuan@users.noreply.github.com>
2015-01-05 20:12:49 -0800

    update production url

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://tuan-blog-vi.azurewebsites.net',
+        url: 'http://tuan-blog.azurewebsites.net',
         mail: {
           transport: 'SMTP',
           options: {
