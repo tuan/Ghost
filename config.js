50de5ef2769c6597396a8cf4234ff2f2553964cc
Tuan Nguyen <tuan@users.noreply.github.com>
2015-01-06 16:45:59 -0800

    setup sendgrid account

--- config.js
+++ config.js
@@ -14,6 +14,7 @@ config = {
         mail: {
           transport: 'SMTP',
           options: {
+            service: 'Sendgrid',
             auth: {
               user: process.env.SMTP_USER,
               pass: process.env.SMTP_PASS
